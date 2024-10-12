340995087537a194cc4cad38b0043f5ddc7e2567
richjameslozano <richjameslozano@gmail.com>
2024-10-11 18:22:16 -0700

    First Commit

---
+++ ticketing-system/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <p>
+          Hello Guys!
+        </p>
+  
+     
+      </header>
+    </div>
+  );
+}
+
+export default App;
