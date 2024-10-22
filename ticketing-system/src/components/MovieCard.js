e95c7eae57b78b4b305cbde40f284c8584ea9338
RichiRich0928 <henreizharuta@gmail.com>
2024-10-22 19:13:12 +0800

    MOVIE CARDS MERON NAA

--- ticketing-system/src/components/MovieCard.js
+++ ticketing-system/src/components/MovieCard.js
@@ -2,22 +2,20 @@ import React from 'react';
 import { Card, Button } from 'antd';
 import '../css/componentsStyle/MovieCard.css'; // Import the CSS file
 
-const MovieCard = ({ title, screen, price, image }) => (
-  // <Card
-  //   hoverable
-  //   className="movie-card"
-  //   cover={<img alt={title} src={image} />}
-  // >
-  //   <Card.Meta title={title} description={`Screen: ${screen}`} className="movie-card-meta" />
-  //   <div className="movie-card-price">{price}$</div>
-  //   <Button type="primary" className="movie-card-button">Buy Tickets</Button>
-  // </Card>
+const MovieCard = ({ title, date, price, image, venue }) => (
 
   <div className='movie-card-container'>
     <div>
-      <div>{price}</div>
+      <img src={image} className='card-posters'></img>
     </div>
+    <div className='details'>
+      <div className='deet-title'>{title}</div>
+      <div className='deet-date'>{date}</div>
+      <div className='deet-venue'>{venue}</div>
+    </div>
+    
   </div>
 );
+
 
 export default MovieCard;
