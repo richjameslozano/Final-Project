f5701a6b0d7eaf836fd4eb6042afa08414c1f99b
berlene <berlenebernabe12@gmail.com>
2024-10-23 19:28:41 +0800

    Display username sa header

--- ticketing-system/src/signup/SignUp.js
+++ ticketing-system/src/signup/SignUp.js
@@ -44,7 +44,7 @@ const SignUp = () => {
  
         setLoading(true);
         try {
-            const response = await axios.post('http://localhost:8020/signup', formData);
+            const response = await axios.post('http://localhost:8021/signup', formData);
             if (response.status === 201) {
                 message.success('User registered successfully');
             }
