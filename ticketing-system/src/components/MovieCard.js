1f1aaa0b903e56db159df9b456f3819db41664d2
berlene <berlenebernabe12@gmail.com>
2024-10-23 11:37:54 +0800

    may hover

--- ticketing-system/src/components/MovieCard.js
+++ ticketing-system/src/components/MovieCard.js
@@ -2,18 +2,23 @@ import React from 'react';
 import { Card, Button } from 'antd';
 import '../css/componentsStyle/MovieCard.css'; // Import the CSS file
 
+
 const MovieCard = ({ title, date, price, image, venue }) => (
 
   <div className='movie-card-container'>
-    <div>
-      <img src={image} className='card-posters'></img>
+      <div className='button-container'>
+      <Button type='primary'>Buy Tickets</Button>
+    </div>
+    <div className='card-posters'>
+      <img src={image} className='images'></img>
+
     </div>
     <div className='details'>
       <div className='deet-title'>{title}</div>
       <div className='deet-date'>{date}</div>
       <div className='deet-venue'>{venue}</div>
     </div>
-    
+  
   </div>
 );
 
